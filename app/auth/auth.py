580c79b5c833dba1a9e4bd08bd7b360b01645c12
vlkttlv <vitavitavita04@bk.ru>
2025-03-26 11:30:10 +0700

    auth

---
+++ app/auth/auth.py
@@ -0,0 +1,68 @@
+from datetime import datetime, timedelta
+import jwt
+from pydantic import EmailStr
+from passlib.context import CryptContext
+from app.exceptions import IncorrectEmailOrPasswordException
+from app.config import settings
+from app.user.service import TokenService, UsersService
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_DAYS = 7
+
+def get_password_hash(password: str) -> str:
+    """Генерирует хэшированный пароль"""
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password) -> bool:
+    """
+    Проверяет пароль на валидность
+
+    -Аргументы:
+        plain_password: пароль пользователя в обычном виде
+        hashed_password: захэшированный пароль
+    """
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    """Создает access токен"""
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, settings.SECRET_KEY, settings.ALGORITHM)
+
+
+async def create_refresh_token(data: dict) -> str:
+    """Создает refresh токен"""
+    to_encode = data.copy()
+    now_time = datetime.utcnow()
+    expire = now_time + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire})
+    token = jwt.encode(to_encode, settings.SECRET_KEY, settings.ALGORITHM)
+    user_id = int(data["sub"])
+
+    # Проверяем, есть ли уже такой refresh токен в БД
+    token_user = await TokenService.find_one_or_none(user_id=user_id)
+    if not token_user:
+        await TokenService.add(token=token, user_id=user_id, created_at=now_time, expires_at=expire)
+    else:
+        await TokenService.update_token(created_at=now_time, expires_at=expire, user_id=user_id)
+    return token
+
+
+async def authenticate_user(email: EmailStr, password: str):
+    """
+    Аутенфицирует пользователя
+    
+    Проверяет, есть ли пользователь с такой почтой в БД и совпадает ли введенный пароль
+    
+    -Возвращает:
+        Optional[users]: Экземпляр модели пользователя, если запись найдена.
+        Если запись не найдена, возвращается None.
+    """
+    user = await UsersService.find_one_or_none(email=email)
+    if not (user and verify_password(password, user.hashed_password)):
+        raise IncorrectEmailOrPasswordException
+    return user
---
+++ app/auth/dependencies.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+import jwt
+from fastapi import Depends, HTTPException, Request
+from jwt.exceptions import InvalidTokenError
+from app.config import settings
+from app.exceptions import (IncorrectRoleException, IncorrectTokenFormatException,
+                            TokenAbsentException,
+                            TokenExpiredException,
+                            UserIsNotPresentException)
+from app.user.service import TokenService, UsersService
+
+
+def get_token(request: Request):
+    """Получение текущего токена из кук"""
+    token = request.cookies.get("access_token")
+    if not token:
+        raise TokenAbsentException
+    return token
+
+async def get_refresh_token(token: str = Depends(get_token)):
+    """Метод, получающий refresh токен"""
+    # декодируем текущий access токен без проверки подписи и времени
+    try:
+        payload = jwt.decode(token, options={"verify_signature": False, "verify_exp": False})
+    except InvalidTokenError as e:
+        raise IncorrectTokenFormatException from e
+    user_id: str = payload.get("sub")
+    if not user_id:
+        raise UserIsNotPresentException
+    # находим refresh токен для текущего пользователя
+    refresh_user = await TokenService.find_one_or_none(user_id=int(user_id))
+    # если refresh токен просрочен, то выбрасываем исключение
+    if datetime.utcnow().timestamp() > refresh_user.expires_at.timestamp():
+        raise HTTPException(status_code=401)
+    refresh_token = refresh_user.token
+    return refresh_token
+
+
+async def get_current_user(token: str = Depends(get_token)):
+    """Возвращает текущего пользователя"""
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, settings.ALGORITHM
+        )
+    except InvalidTokenError as e:
+        raise IncorrectTokenFormatException from e
+    expire: str = payload.get("exp")
+    if (not expire) or (int(expire) < datetime.utcnow().timestamp()):
+        raise TokenExpiredException
+    user_id: str = payload.get("sub")
+    if not user_id:
+        raise UserIsNotPresentException
+    user = await UsersService.find_one_or_none(id = int(user_id))
+    if not user:
+        raise UserIsNotPresentException
+    return user
+
+
+async def get_role(current_user = Depends(get_current_user)):
+    return current_user.role
+
+async def check_tutor_role(current_role = Depends(get_role)):
+    if current_role != "TUTOR":
+        raise IncorrectRoleException
