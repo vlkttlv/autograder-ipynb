bd7887568edfec21185226a37f244ab2705515a4
vlkttlv <vitavitavita04@bk.ru>
2025-04-01 13:37:21 +0700

    админка

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,10 +1,11 @@
 from sqladmin import ModelView
 from app.assignment.models import Assignments 
 from app.user.models import RefreshToken, Users
+from app.submissions.models import Submissions
 
 
 class UsersAdmin(ModelView, model=Users):
-    column_list = [Users.id, Users.email, Users.role]
+    column_list = [Users.id, Users.first_name, Users.last_name, Users.email, Users.role]
     column_details_exclude_list = [Users.hashed_password]
     name = "Пользователь"
     name_plural = "Пользователи"
@@ -19,3 +20,9 @@ class AssignmentsAdmin(ModelView, model=Assignments):
     column_list = [c.name for c in Assignments.__table__.c] + [Assignments.user]
     name = "Задание"
     name_plural = "Задания"
+
+
+class SubmissionsAdmin(ModelView, model=Submissions):
+    column_list = [c.name for c in Submissions.__table__.c] + [Submissions.user]
+    name = "Решение"
+    name_plural = "Решения"
