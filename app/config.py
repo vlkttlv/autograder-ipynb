a6475c63b36fa0740542ef874a49b3ae36ea2fbe
vlkttlv <vitavitavita04@bk.ru>
2025-04-10 16:44:53 +0700

    config

--- app/config.py
+++ app/config.py
@@ -12,6 +12,12 @@ class Settings(BaseSettings):
     DB_PASS: str
     DB_NAME: str
 
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
+
     SECRET_KEY: str
     ALGORITHM: str
 
@@ -19,6 +25,10 @@ class Settings(BaseSettings):
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
+    @property
+    def TEST_DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
+    
     class Config:
         env_file = ".env"
 
