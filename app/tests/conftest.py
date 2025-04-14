c43903a7a7befdfe3569d9d836c67f166de3e3e9
vlkttlv <vitavitavita04@bk.ru>
2025-04-14 12:41:04 +0700

    commit

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -21,7 +21,6 @@ configure_logging()
 @pytest.fixture(autouse=True, scope="session")
 async def prepare_database():
     assert settings.MODE == "TEST"
-    logger.info("!!!!!!" + DATABASE_URL)
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
