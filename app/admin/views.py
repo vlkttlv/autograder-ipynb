8260e269e86bf00a45e50395b04cc2a1901354a1
vlkttlv <vitavitavita04@bk.ru>
2025-10-22 11:54:00 +0700

    обновила админку

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,28 +1,49 @@
 from sqladmin import ModelView
-from app.assignment.models import Assignments 
-from app.user.models import RefreshToken, Users
-from app.submissions.models import Submissions
+from app.assignment.models import AssignmentFile, Assignments 
+from app.user.models import Disciplines, Groups, RefreshToken, Users
+from app.submissions.models import SubmissionFiles, Submissions
 
 
 class UsersAdmin(ModelView, model=Users):
-    column_list = [Users.id, Users.first_name, Users.last_name, Users.email, Users.role]
+    column_list = [Users.id, Users.first_name, Users.last_name, Users.email, Users.role, Users.group_id, Users.group]
     column_details_exclude_list = [Users.hashed_password]
     name = "Пользователь"
     name_plural = "Пользователи"
 
 
 class RefreshTokensAdmin(ModelView, model=RefreshToken):
-    column_list = [c.name for c in RefreshToken.__table__.c]
+    column_list = [c.name for c in RefreshToken.__table__.c] + [RefreshToken.user]
     name = "Токен"
     name_plural = "Токены"
+
+class GroupsAdmin(ModelView, model=Groups):
+    column_list = [c.name for c in Groups.__table__.c] + [Groups.students]
+    name = "Группа"
+    name_plural = "Группы"
+
+class DisciplinesAdmin(ModelView, model=Disciplines):
+    column_list = [c.name for c in Disciplines.__table__.c] + [Disciplines.teacher]
+    name = "Дисциплина"
+    name_plural = "Дисциплины"
 
 class AssignmentsAdmin(ModelView, model=Assignments):
     column_list = [c.name for c in Assignments.__table__.c] + [Assignments.user]
     name = "Задание"
     name_plural = "Задания"
 
+class AssignmentFilesAdmin(ModelView, model=AssignmentFile):
+    column_list = [c.name for c in AssignmentFile.__table__.c] + [AssignmentFile.assignment]
+    name = "Файл задания"
+    name_plural = "Файлы заданий"
+
 
 class SubmissionsAdmin(ModelView, model=Submissions):
     column_list = [c.name for c in Submissions.__table__.c] + [Submissions.user]
     name = "Решение"
     name_plural = "Решения"
+
+
+class SubmissionFilesAdmin(ModelView, model=SubmissionFiles):
+    column_list = [c.name for c in SubmissionFiles.__table__.c] + [SubmissionFiles.submission]
+    name = "Файл решения"
+    name_plural = "Файлы решений"
--- app/main.py
+++ app/main.py
@@ -7,22 +7,21 @@ from app.admin.views import (
     SubmissionsAdmin,
     UsersAdmin,
     RefreshTokensAdmin,
+    AssignmentFilesAdmin,
+    GroupsAdmin,
+    DisciplinesAdmin,
+    SubmissionFilesAdmin
 )
-
-# from app.middleware.auth import TokenRefreshMiddleware
 from app.user.router import router as user_router
 from app.assignment.router import router as assignment_router
 from app.submissions.router import router as submission_router
 from app.submissions.router import sub_router as two_submission_router
 from app.pages.router import router as frontend
 from app.admin.auth import authentication_backend
-
 from app.db import engine
 
 
 app = FastAPI()
-
-# app.add_middleware(TokenRefreshMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
@@ -44,6 +43,12 @@ admin.add_view(UsersAdmin)
 admin.add_view(AssignmentsAdmin)
 admin.add_view(RefreshTokensAdmin)
 admin.add_view(SubmissionsAdmin)
+admin.add_view(AssignmentFilesAdmin)
+admin.add_view(GroupsAdmin)
+admin.add_view(DisciplinesAdmin)
+admin.add_view(SubmissionFilesAdmin)
 
 app.mount("/static", StaticFiles(directory="app/static"), "static")
-app.mount("/assignment", StaticFiles(directory="app/assignment"), name="assignment")
+app.mount(
+    "/assignment", StaticFiles(directory="app/assignment"), name="assignment"
+)
