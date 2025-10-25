49514e9ef9fdfb4eb88695b79f1cc06052f4405e
vlkttlv <vitavitavita04@bk.ru>
2025-10-25 10:31:56 +0700

    добавила валидаторы в схему

--- app/user/schemas.py
+++ app/user/schemas.py
@@ -1,5 +1,6 @@
 from enum import Enum
-from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+from pydantic import BaseModel, EmailStr, Field, model_validator, field_validator
 
 
 class UserRole(str, Enum):
@@ -17,14 +18,39 @@ class UserLoginSchema(UserBaseSchema):
 
 
 class UserRegisterSchema(UserBaseSchema):
-    first_name: str
-    last_name: str
+    first_name: str = Field(..., min_length=2, max_length=50, description="Имя")
+    last_name: str = Field(..., min_length=2, max_length=50, description="Фамилия")
+    role: UserRole
+    group: Optional[str] = Field(default=None,
+                                 min_length=4,
+                                 max_length=7,
+                                 description="Номер группы, например, 8В34")
+    
+
+    @field_validator('group', mode='before')
+    def check_group_first_char(cls, v, info):
+        role = info.data.get('role')
+        if role == UserRole.STUDENT:
+            if not v:
+                raise ValueError("Для студента необходимо указать группу.")
+            if not v[0].isdigit():
+                raise ValueError("Группа должна начинаться с цифры.")
+        return v
+    
+    @field_validator('group')
+    @classmethod
+    def normalize_group(cls, v, info):
+        """Приведение группы к верхнему регистру"""
+        role = info.data.get('role')
+        if role == 'STUDENT' and v is not None:
+            return v.upper().strip()
+        return None
 
 
 class UserTestRegisterSchemas(UserRegisterSchema):
     role: UserRole
 
-    
+
 class UserResponseSchema(BaseModel):
     id: int
     role: UserRole
