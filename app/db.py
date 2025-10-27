a23dda207191690de65cb06a852184005b5206d1
vlkttlv <vitavitavita04@bk.ru>
2025-10-27 10:18:47 +0700

    рефакторинг

--- app/db.py
+++ app/db.py
@@ -1,23 +1,26 @@
+from sqlalchemy import NullPool
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-from sqlalchemy import NullPool, Pool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from app.config import settings
 
 if settings.MODE == "TEST":
     DATABASE_URL = settings.TEST_DATABASE_URL
-    DATABASE_PARAMS = {'poolclass': NullPool}
+    DATABASE_PARAMS = {"poolclass": NullPool}
 else:
     DATABASE_URL = settings.DATABASE_URL
     DATABASE_PARAMS = {
-        'pool_size': 10,  # количество соединений в пуле
-        'max_overflow': 20,  # максимальное количество соединений, которое может быть создано при превышении pool_size
-        'pool_timeout': 30,  # время ожидания, чтобы получить соединение из пула
-        'pool_recycle': 3600  # время, через которое соединения будут сбрасываться
+        "pool_size": 10,
+        "max_overflow": 20,
+        "pool_timeout": 30,
+        "pool_recycle": 3600,
     }
 
-engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)  
+engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
 
-async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session_maker = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
 
 class Base(DeclarativeBase):
     pass
--- app/exceptions.py
+++ app/exceptions.py
@@ -8,24 +8,30 @@ class AutograderException(HTTPException):
     def __init__(self):
         super().__init__(status_code=self.status_code, detail=self.detail)
 
+
 class IncorrectEmailOrPasswordException(AutograderException):
     status_code = status.HTTP_401_UNAUTHORIZED
     detail = "Неверная почта или пароль"
+
 
 class IncorrectTokenFormatException(AutograderException):
     status_code = status.HTTP_401_UNAUTHORIZED
     detail = "Неверный формат токена"
 
+
 class TokenAbsentException(AutograderException):
     status_code = status.HTTP_401_UNAUTHORIZED
     detail = "Токен отсутствует"
 
+
 class TokenExpiredException(AutograderException):
     status_code = status.HTTP_401_UNAUTHORIZED
     detail = "Срок действия токена истек"
-    
+
+
 class UserIsNotPresentException(AutograderException):
     status_code = status.HTTP_401_UNAUTHORIZED
+
 
 class UserAlreadyExistsException(AutograderException):
     status_code = status.HTTP_409_CONFLICT
@@ -35,15 +41,17 @@ class UserAlreadyExistsException(AutograderException):
 class IncorrectFormatAssignmentException(AutograderException):
     status_code = status.HTTP_400_BAD_REQUEST
     detail = "Файл должен быть в формате .ipynb"
-    
+
 
 class NotFoundSolutionsInAssignmentException(AutograderException):
     status_code = status.HTTP_400_BAD_REQUEST
     detail = "Не найдены решения"
 
+
 class NotFoundTestsInAssignmentException(AutograderException):
     status_code = status.HTTP_400_BAD_REQUEST
     detail = "Не найдены скрытые тесты"
+
 
 class IncorrectRoleException(AutograderException):
     status_code = status.HTTP_400_BAD_REQUEST
@@ -69,7 +77,7 @@ class WgongDateException(AutograderException):
     status_code = status.HTTP_400_BAD_REQUEST
     detail = "Неверная дата"
 
-    
+
 class SolutionNotFoundException(AutograderException):
     status_code = status.HTTP_404_NOT_FOUND
     detail = "Решение не найдено"
