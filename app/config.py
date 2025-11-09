e84344d8bc9a8b6030c9d892e35e85ac8bba656c
vlkttlv <vitavitavita04@bk.ru>
2025-11-09 19:38:22 +0700

    update config

--- app/config.py
+++ app/config.py
@@ -20,6 +20,13 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
 
+    GOOGLE_REDIRECT_URI: str
+    CLIENT_SECRETS_FILE: str
+
+    DROPBOX_REFRESH_TOKEN: str
+    DROPBOX_APP_KEY: str
+    DROPBOX_APP_SECRET: str
+
     @property
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
