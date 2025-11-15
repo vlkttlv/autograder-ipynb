6b2ee325ff3872e2e19765c8063021aafde46c3b
vlkttlv <vitavitavita04@bk.ru>
2025-11-15 16:39:50 +0700

    update assignment and assignment_file models

--- app/assignment/models.py
+++ app/assignment/models.py
@@ -29,9 +29,11 @@ class Assignments(Base):
     grade: Mapped[int]
     created_at: Mapped[datetime] = mapped_column(DateTime)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    discipline_id: Mapped[int] = mapped_column(ForeignKey("disciplines.id"), nullable=False)
 
     # Связи с другими таблицами
-    user = relationship("Users", back_populates="assignment")
+    user = relationship("Users", back_populates="assignments")
+    discipline = relationship("Disciplines")
     submission = relationship("Submissions", back_populates="assignment")
     assignment_files = relationship(
         "AssignmentFile", back_populates="assignment"
@@ -44,8 +46,9 @@ class AssignmentFile(Base):
     id: Mapped[int] = mapped_column(
         Integer, primary_key=True, autoincrement=True
     )
-    assignment_id = mapped_column(ForeignKey("assignments.id"))
-    file_type = mapped_column(String)  # original / modified
-    content = mapped_column(LargeBinary)
+    assignment_id: Mapped[UUID] = mapped_column(ForeignKey("assignments.id"))
+    file_type: Mapped[str] = mapped_column(String)  # original / modified
+    file_id: Mapped[str] = mapped_column(String)  # путь в Dropbox
+    file_link: Mapped[str] = mapped_column(String, nullable=False)  # публичная ссылка
 
     assignment = relationship("Assignments", back_populates="assignment_files")
