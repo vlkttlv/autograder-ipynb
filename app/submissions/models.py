f2ad6fa0edc4317193331f40902a07c20565ab17
vlkttlv <vitavitavita04@bk.ru>
2025-11-19 10:07:15 +0700

    update models

--- app/submissions/models.py
+++ app/submissions/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import JSON, ForeignKey, Integer, LargeBinary
+from datetime import datetime
+from sqlalchemy import JSON, DateTime, ForeignKey, Integer, LargeBinary, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.dialects.postgresql import UUID
 from uuid import uuid4
@@ -15,9 +16,10 @@ class Submissions(Base):
     score: Mapped[int] = mapped_column(Integer)
     number_of_attempts: Mapped[int]
     feedback: Mapped[dict | None] = mapped_column(JSON, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
     # Связи с другими таблицами
-    user = relationship("Users", back_populates="submission")
+    user = relationship("Users", back_populates="submissions")
     assignment = relationship("Assignments", back_populates="submission")
     submission_files = relationship("SubmissionFiles", back_populates="submission")
 
@@ -26,8 +28,9 @@ class SubmissionFiles(Base):
     __tablename__ = "submission_files"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    submission_id = mapped_column(ForeignKey("submission.id"))
-    assignment_id = mapped_column(ForeignKey("assignments.id"))
-    content = mapped_column(LargeBinary)
+    submission_id: Mapped[UUID] = mapped_column(ForeignKey("submission.id", ondelete="CASCADE"))
+    assignment_id: Mapped[UUID] = mapped_column(ForeignKey("assignments.id"))
+    file_id: Mapped[str] = mapped_column(String)  # путь в Dropbox
+    file_link: Mapped[str] = mapped_column(String, nullable=False)  # публичная ссылка
 
     submission = relationship("Submissions", back_populates="submission_files")
